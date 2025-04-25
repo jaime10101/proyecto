15dd9b8a53cb9b9e5d679ff7c39aebe64b596124
jaime10101 <jeherrera28@hotmail.com>
2025-04-25 23:07:52 +0200

    Add files via upload

---
+++ tienda_proyecto/view/app.js
@@ -0,0 +1,18 @@
+function guardarAlmacenamientoLocal(llave, valor_a_guardar) {
+    localStorage.setItem(llave, JSON.stringify(valor_a_guardar))
+}
+function obtenerAlmacenamientoLocal(llave) {
+    const datos = JSON.parse(localStorage.getItem(llave))
+    return datos
+}const header = document.querySelector("#header");
+const contenedor = document.querySelector("#contenedor");
+const body = document.querySelector("body");
+window.addEventListener("scroll", function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
+    }
+    else{
+        header.classList.remove("scroll")
+    }
+})
+
